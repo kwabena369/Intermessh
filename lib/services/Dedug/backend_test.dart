ea4b9ed96bc9063ec9f7584fefa727a8b396fa91
kwabena369 <68285748+kwabena369@users.noreply.github.com>
2024-10-01 12:26:07 +0000

    crystal_point

---
+++ lib/services/Dedug/backend_test.dart
@@ -0,0 +1,15 @@
+//  checking  the communication with the vercel real time thing
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+//  the function for various debug function
+class Classic_debug {
+//   the base url
+  static const String base_url = 'https://intermessh-backend.vercel.app';
+
+//  method for basic console.log
+  Future<void> Check_try_two() async {
+    //  makng the get called
+    http.Response responce = await http.get( Uri.parse('$base_url/testing'));
+  }
+}
